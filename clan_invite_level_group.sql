0333358d2d607d763529669d4f6322c88f6db533
Ruiqi Mao (Bot) <ruiqim+bot@gmail.com>
2021-03-04 17:15:13 +0000

    10000410

---
+++ clan_invite_level_group.sql
@@ -0,0 +1,16 @@
+CREATE TABLE 'clan_invite_level_group' ('level_group_id' INTEGER NOT NULL, 'team_level_from' INTEGER NOT NULL, 'team_level_to' INTEGER NOT NULL, PRIMARY KEY('level_group_id'));
+INSERT INTO "clan_invite_level_group" VALUES(1,1,14);
+INSERT INTO "clan_invite_level_group" VALUES(2,15,20);
+INSERT INTO "clan_invite_level_group" VALUES(3,21,25);
+INSERT INTO "clan_invite_level_group" VALUES(4,26,30);
+INSERT INTO "clan_invite_level_group" VALUES(5,31,35);
+INSERT INTO "clan_invite_level_group" VALUES(6,36,40);
+INSERT INTO "clan_invite_level_group" VALUES(7,41,45);
+INSERT INTO "clan_invite_level_group" VALUES(8,46,50);
+INSERT INTO "clan_invite_level_group" VALUES(9,51,55);
+INSERT INTO "clan_invite_level_group" VALUES(10,56,60);
+INSERT INTO "clan_invite_level_group" VALUES(11,61,65);
+INSERT INTO "clan_invite_level_group" VALUES(12,66,70);
+INSERT INTO "clan_invite_level_group" VALUES(13,71,75);
+INSERT INTO "clan_invite_level_group" VALUES(14,76,80);
+INSERT INTO "clan_invite_level_group" VALUES(15,81,-1);
---
+++ room_item_announcement.sql
@@ -0,0 +1,2 @@
+CREATE TABLE 'room_item_announcement' ('id' INTEGER NOT NULL, 'announcement_start' TEXT NOT NULL, 'announcement_end' TEXT NOT NULL, 'announcement_text' TEXT NOT NULL, PRIMARY KEY('id'));
+INSERT INTO "room_item_announcement" VALUES(2,'2018/10/18 15:00:00','2018/11/19 14:59:59','If you purchase the newly-added Memorial Jukebox furniture item, you will be able to change the background music on the home page and in the Guildhouse, and even purchase brand new tracks to use. Check it out in the furniture shop!');
