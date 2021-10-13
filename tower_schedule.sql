6c01290c79ee8a5db95c5b08bfdb6f75c0ee65ee
Ruiqi Mao (Bot) <ruiqim+bot@gmail.com>
2021-10-13 09:05:31 +0000

    10003300

--- tower_schedule.sql
+++ tower_schedule.sql
@@ -1,2 +1,3 @@
 CREATE TABLE 'tower_schedule' ('tower_schedule_id' INTEGER NOT NULL, 'max_tower_area_id' INTEGER NOT NULL, 'opening_story_id' INTEGER NOT NULL, 'count_start_time' TEXT NOT NULL, 'recovery_disable_time' TEXT NOT NULL, 'start_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL, PRIMARY KEY('tower_schedule_id'));
 INSERT INTO "tower_schedule" VALUES(1001,1,7001000,'2021/09/14 15:59:59','2021/09/15 13:00:00','2021/09/10 13:00:00','2021/09/15 15:59:59');
+INSERT INTO "tower_schedule" VALUES(1002,2,7002000,'2021/10/17 15:59:59','2021/10/18 13:00:00','2021/10/13 13:00:00','2021/10/18 15:59:59');
